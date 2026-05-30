389bc8de88490b8c2e1b357d072a682420e50064
Nicco <nicco@example.com>
2026-05-30 21:34:38 +0200

    chore: default log level to warn instead of info

--- src/main.rs
+++ src/main.rs
@@ -167,7 +167,7 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("warn")),
         )
         .init();
 
